2cf06faecbcce676b2df152b54745177f601066c
VIC0143562 <danielbsilva130@gmail.com>
2023-08-21 11:41:39 -0300

    lista

---
+++ laco_live.py
@@ -0,0 +1,11 @@
+'''
+exibir todos os numeros existentes na sequencia 1 a 500
+'''
+
+contador = 1
+
+while (contador <= 500):
+    print(f"contador: {contador}")
+    contador = contador + 1
+
+print("Depois do bloco while")
---
+++ lista051/questao04.py
@@ -0,0 +1,11 @@
+'''Desenvolver um programa que apresente o valor da soma dos cem primeiros números inteiros (1 + 2 + 3 + 4 + ...
++ 97 + 98 + 99 + 100)'''
+
+soma = 0
+numero = 1
+
+while numero <= 100:
+    soma += numero
+    numero += 1
+
+print(f"O valor da soma dos cem primeiros números inteiros é {soma}")
